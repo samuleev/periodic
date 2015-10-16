da90f6a0bd821a55ef40ecbabdd9f173bce49473
ArtemKhrienov <artemkhrenov@gmail.com>
2015-10-16 16:23:29 +0300

    Main styles 1.0.2

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -7,8 +7,8 @@
     <link href="{{ asset('/css/bootstrap.min.css') }}" rel="stylesheet" type="text/css" />
     <!-- Main Stylesheets -->
     <link href="{{ asset('/css/main.css') }}" rel="stylesheet" type="text/css" />
-    <!-- Awesome Icons -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">    
+    <!-- Awesome Icons 
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css"> -->   
 </head>
 <body>
 @include('layouts.header')
