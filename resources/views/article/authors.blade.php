64c009468aa10f3541d5c7ed79285cd43541787b
Andrey Samuleev <andrey@samuleev.com>
2015-10-27 14:05:08 +0200

    fix spaces in authors list views

--- resources/views/article/authors.blade.php
+++ resources/views/article/authors.blade.php
@@ -2,6 +2,5 @@
     @if(isset($author->name)){{{$author->name}}}.
     @if(isset($author->patronymic)){{{$author->patronymic}}}.@endif
     @endif
-    {{{$author->surname}}}
-    @if($authorIndex < (count($article->authors) - 1)),@endif
+    {{{$author->surname}}}@if($authorIndex < (count($article->authors) - 1)),@endif
 @endforeach
