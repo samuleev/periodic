b953651ad631f89325226b8ae37f1d2d5a3e107e
ArtemKhrienov <artemkhrenov@gmail.com>
2015-12-22 16:02:25 +0200

    search page

--- resources/views/search/index.blade.php
+++ resources/views/search/index.blade.php
@@ -11,74 +11,103 @@
 @stop
 
 @section('content')
-    @if (count($errors) > 0)
-        <div class="alert alert-danger">
-            <ul>
-                @foreach ($errors->all() as $error)
-                    <li>{{ $error }}</li>
-                @endforeach
-            </ul>
-        </div>
-    @endif
-    <div class="container main-content">
+
+    <div class="container main-content" style="padding-top:21px;">
+        @if (count($errors) > 0)
+            <div class="row">
+                    <div class="alert alert-danger" style="font-style:italic;">
+                        @foreach ($errors->all() as $error)
+                            {{ $error }}
+                        @endforeach
+                    </div>
+            </div>
+        @endif
         <div class="row">
             <div class="col-md-6">
-                <h3>Пошук публікацій</h3>
+                <h4>Форма пошуку</h4>
+                <p>За допомогою форми пошуку є змога знайти публікації, що Вас зацікавили за мінімальну кількість часу.</p>
+                <p><strong>Вимоги до заповнення форми пошуку:</strong></p>
+                <p>Назва публікації - повне або часткове найменування публікації.</p>
+                <p>Прізвище автора - повне або часткове прізвище автора публікації.</p>
+                <p>Рік видання - рік коли було опубліковано у форматі "1988".</p>
+                <p><i>Для здійснення пошуку необхідно заповнити хоча б одне поле форми.</i></p>
             </div>
-        </div>
-        <div class="row top10">
             <div class="col-md-6 well">
-                {!! Form::open(array('id' => 'searchForm', 'route' => 'search.process')) !!}
+                <div class="row">
+                    <div class="col-md-12">
+                        {!! Form::open(array('id' => 'searchForm', 'route' => 'search.process')) !!}
 
-                {!! Form::label('name', 'Назва публікації', array('class'=>'control-label')) !!}
-                @if(isset($name))
-                    {!! Form::text('name', $name, array('class'=>'form-control')) !!}
-                @else
-                    {!! Form::text('name', null, array('class'=>'form-control')) !!}
-                @endif
+                        {!! Form::label('name', 'Назва публікації', array('class'=>'control-label')) !!}
+                        @if(isset($name))
+                            {!! Form::text('name', $name, array('class'=>'form-control')) !!}
+                        @else
+                            {!! Form::text('name', null, array('class'=>'form-control')) !!}
+                        @endif
+                    </div>
+                </div>
 
-                <br />
-                {!! Form::label('author', 'Прізвище автора', array('class'=>'control-label')) !!}
-                @if(isset($author))
-                    {!! Form::text('author', $author, array('class'=>'form-control')) !!}
-                @else
-                    {!! Form::text('author', null, array('class'=>'form-control')) !!}
-                @endif
+                <div class="row top10">
+                    <div class="col-md-6">
+                        {!! Form::label('author', 'Прізвище автора', array('class'=>'control-label')) !!}
+                        @if(isset($author))
+                            {!! Form::text('author', $author, array('class'=>'form-control')) !!}
+                        @else
+                            {!! Form::text('author', null, array('class'=>'form-control')) !!}
+                        @endif
+                    </div>
+                    <div class="col-md-6">
+                        {!! Form::label('year', 'Рік видання', array('class'=>'control-label')) !!}
+                        @if(isset($year))
+                            {!! Form::text('year', $year, array('class'=>'form-control')) !!}
+                        @else
+                            {!! Form::text('year', null, array('class'=>'form-control')) !!}
+                        @endif
+                    </div>
+                </div>
 
-                <br />
-                {!! Form::label('year', 'Рік видання', array('class'=>'control-label')) !!}
-                @if(isset($year))
-                    {!! Form::text('year', $year, array('class'=>'form-control')) !!}
-                @else
-                    {!! Form::text('year', null, array('class'=>'form-control')) !!}
-                @endif
-                <br />
+                <div class="row" style="margin-top:21px;">
+                    <div class="col-md-12">
+                        {!! Form::submit('Почати пошук', array('class'=>'btn btn-default', 'style' => 'float: right;')) !!}
+                        {!! Form::close() !!}
+                    </div>
+                </div>
 
-                {!! Form::submit('Пошук', array('class'=>'btn btn-default', 'style' => 'float: right;')) !!}
-
-                {!! Form::close() !!}
             </div>
         </div>
 
         @if((!empty($name) || !empty($author) || !empty($year)) && (!isset($articles) || count($articles) == 0))
             <div class="row top10">
-                <div class="col-md-12">
-                    <h4>За вказаними критеріями публікаціїй не знайдено.</h4>
+                <div class="col-md-12 text-center" style="background: url(/public/img/icon-line.png) repeat-x;"><span style="font-size:20px;" class="glyphicon glyphicon-search" aria-hidden="true"></span></div>
+                <div class="col-md-12" style="margin:21px 0 0 0;">
+                    <div class="alert alert-info" role="alert"><strong><i>За вказаними критеріями публікаціїй не знайдено.</i></strong></div>
                 </div>
             </div>
         @endif
 
         @if(isset($articles))
-            @foreach($articles as $article)
-                <div class="row top10">
-                    <div class="col-md-12">
-                        <a href="{{{ route('article.details', array($article->article_id)) }}}">{{{ $article->name.'.' }}}</a>
-                        &nbsp;@if(count($article->authors)>0)/&nbsp;@include('article.authors')&nbsp;@endif// {{{ $article->journal_name }}}. - {{{$article->issue_year}}}. - № {{{$article->number_in_year}}}.
-                    </div>
-                </div>
-            @endforeach
+                <div class="col-md-12 text-center" style="background: url(/public/img/icon-line.png) repeat-x; margin:21px 0;"><span style="font-size:18px;" class="glyphicon glyphicon-arrow-down" aria-hidden="true"></span></div>
             <div class="row top10">
-                <div class="col-md-12">
+                    <table class="table table-hover">
+                        <thead>
+                            <th style="text-align:center;">Назва публікації</th>
+                            <th style="text-align:center;">Автори публікації</th>
+                            <th style="text-align:center;">Назва журналу</th>
+                            <th style="text-align:center;">Рік / Випуск</th>
+                        </thead>
+                        <tbody>
+                        @foreach($articles as $article)
+                                <tr>
+                                    <td style="width:45%;"><i><a href="{{{ route('article.details', array($article->article_id)) }}}">{{{ $article->name.'.' }}}</a></i></td>
+                                    <td style="width:20%; text-align:center;"><small>@if(count($article->authors)>0) @include('article.authors') @endif</small></td>
+                                    <td style="width:25%; text-align:center;"><small>{{{ $article->journal_name }}}</small></td>
+                                    <td style="width:15%; text-align:center;"><small><strong>{{{$article->issue_year}}}</strong></small> / <small><strong>№ {{{$article->number_in_year}}}</strong></small></td>
+                                </tr>
+                        @endforeach
+                        </tbody>
+                    </table>
+            </div>
+            <div class="row top10">
+                <div class="col-md-12 text-center">
                     {!! str_replace('/?', '?', $articles->render()) !!}
                 </div>
             </div>
