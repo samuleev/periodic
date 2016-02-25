19892b3f551ef06ffb4662951296651db3e02356
root <andrey@samuleev.com>
2016-02-25 10:24:32 +0200

    breadcrumb text-overflow

--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -3,7 +3,6 @@
         <div class="row">
             <div class="col-md-12">
                 <ol class="periodic-breadcrumb">
-                    <span class="vw-breadcrumb-title">ВИ ЗНАХОДИТЕСЬ ТУТ:</span>
                     @yield('bread_crumps')
                 </ol>
             </div>
