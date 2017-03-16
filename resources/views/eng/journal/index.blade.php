c34681310b6a45809335bb80c6e9edfb33959582
root <andrey@samuleev.com>
2017-03-16 14:44:46 +0200

    new journals header text

--- resources/views/eng/journal/index.blade.php
+++ resources/views/eng/journal/index.blade.php
@@ -21,15 +21,15 @@
     @if(count($journals))
         <div class="container main-content">
             <div class="row" style="margin-top:21px;">
-                <div class="col-md-4">
+                <div class="col-md-3">
                     <img src={{{ url('/public/img/archive-title-img.jpg') }}}>
                 </div>
-                <div class="col-md-8" style="margin-bottom:21px;">
-                    <p style="text-align:justify;"><strong>University scientific publications have State Registration.</strong> Electronic versions of publications are placed in scientific publications base of Vernadsky National Library of Ukraine.</p>
-                    <p style="text-align:justify;">Four scientific publications are a part of science-metric data base and  <a href="http://old.mon.gov.ua/ua/activity/563/perelik-naukovikh-fakhovikh-vidan/6797/" target="_blank">«List of scientific professional publications of Ukraine, where results of theses for the degree of doctor and candidate»</a>.</p>
-                    <p style="text-align:justify;">KNAFU editors of scientific publications follow  the recommendations of the Ethics SCOPUS publications Committee, and base on the experience of authoritative international  and national journals and founders.</p>
-                    <p><a href="{{{ url('/public/data/publication_ethics_hnups_eng.pdf') }}}" target="_blank">KNAFU ethical rules of scientific publications</a></p>
-                    <p><strong>Contact telephone number:</strong> +38-067-998-02-70</p>
+                <div class="col-md-9" style="margin-bottom:21px;">
+                    <p style="text-align:justify;">All the University scientific publications have State registration and are included into the <a href="http://old.mon.gov.ua/ua/activity/563/perelik-naukovikh-fakhovikh-vidan/6797/" target="_blank">«List of Scientific Professional Publications of Ukraine, where results of final papers for the degree of Doctor of Science and Doctor of Philisophy can be published»</a>.</p>
+                    <p style="text-align:justify;">Abstract information is kept in the National Abstract Database of Vernadskii National Library “Ukrainika naukova” and is publised in the relative subject periodicals of the journal “Dzherelo”.</p>
+                    <p style="text-align:justify;">Publications have the index of international bibliometric and sciencemetric data bases: Academic Resource Index, Google Scholar, Index Copernicus, Open Academic Journals Index, Scientific Indexed Service, Universal Impact Factor.</p>
+                    <p style="text-align:justify;">KhNAFU editorial boards of scientific publications follow the recommendations of the <a href="{{{ url('/public/data/publication_ethics_hnups_eng.pdf') }}}" target="_blank">«The Ethical Rules of KhNAFU Scientific Publications»</a> that meet the requirements of SCOPUS Publishing Ethics Committee, on the experience of authoritative international and national journals and publishing houses.</p>
+                    <p>Scientific publications editorial contact telephone number: (067) 998-02-70, (057) 704-91-97</p>
                 </div>
             </div>
             @foreach($journals as $index => $journal)
--- resources/views/journal/index.blade.php
+++ resources/views/journal/index.blade.php
@@ -14,15 +14,15 @@
     @if(count($journals))
         <div class="container main-content">
             <div class="row" style="margin-top:21px;">
-                <div class="col-md-4">
+                <div class="col-md-3">
                     <img src={{{ url('/public/img/archive-title-img.jpg') }}}>
                 </div>
-                <div class="col-md-8" style="margin-bottom:21px;">
-                    <p style="text-align:justify;"><strong>Наукові видання університету мають державну реєстрацію.</strong> Електронні версії видань своєчасно розміщуються у базі наукових видань Державної бібліотеки імені Вернадського.</p>
-                    <p style="text-align:justify;">Чотири наукових видання університету входять до наукометричних баз даних та до <a href="http://old.mon.gov.ua/ua/activity/563/perelik-naukovikh-fakhovikh-vidan/6797/" target="_blank">«Переліку наукових фахових видань України, в яких можуть публікуватися результати дисертаційних робіт на здобуття наукових ступенів доктора і кандидата наук»</a>.</p>
-                    <p style="text-align:justify;">Редакції наукових видань ХНУПС в своїй роботі суворо керуються рекомендаціями Комітету з етики публікацій SCOPUS, а також спираються на досвід авторитетних міжнародних та вітчизняних журналів та видавництв.</p>
-                    <p><a href="{{{ url('/public/data/publication_ethics_hnups.pdf') }}}" target="_blank">Етичні правила наукових видань ХНУПС</a></p>
-                    <p><strong>Телефони для зв’язку:</strong> (067) 998-02-70, (057) 704-91-97</p>
+                <div class="col-md-9" style="margin-bottom:21px;">
+                    <p style="text-align:justify;">Усі наукові видання університету мають державну реєстрацію та входять до <a href="http://old.mon.gov.ua/ua/activity/563/perelik-naukovikh-fakhovikh-vidan/6797/" target="_blank">„Переліку наукових фахових видань України, в яких можуть публікуватися результати дисертаційних робіт на здобуття наукових ступенів доктора і кандидата наук”</a>.</p>
+                    <p style="text-align:justify;">Реферативна інформація зберігається у загальнодержавній реферативній базі даних Державної бібліотеки імені Вернадського „Україніка наукова” та публікується у відповідних тематичних серіях журналу „Джерело”.</p>
+                    <p style="text-align:justify;">Видання індексуються міжнародними бібліометричними та наукометричними базами даних: Academic Resource Index, Google Scholar, Index Copernicus, Open Academic Journals Index, Scientific Indexed Service, Universal Impact Factor.</p>
+                    <p style="text-align:justify;">Редакційні колегії наукових видань університету в своїй роботі керуються <a href="{{{ url('/public/data/publication_ethics_hnups.pdf') }}}" target="_blank">„Етичними правилами наукових видань ХНУПС”</a>, які відповідають рекомендаціям Комітету з етики публікацій SCOPUS, а також спираються на досвід авторитетних міжнародних та вітчизняних журналів та видавництв.</p>
+                    <p>Контактні телефони редакції наукових видань: (067) 998-02-70, (057) 704-91-97</p>
                 </div>
             </div>
             @foreach($journals as $index => $journal)
